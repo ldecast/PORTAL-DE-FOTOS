d7a41d5251ac987e7499d0654933177b0b7c1637
pabloc54 <3091865790609@ingenieria.usac.edu.gt>
2022-02-25 00:33:20 -0600

    general layout improved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import '@/styles/index.css'
+
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import { NextUIProvider } from '@nextui-org/react';
+
+import { darkTheme } from '@/theme';
 import App from '@/components/App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <NextUIProvider theme={darkTheme}>
+      <App />
+    </NextUIProvider>
+  </StrictMode>,
   document.querySelector('main')
 )
