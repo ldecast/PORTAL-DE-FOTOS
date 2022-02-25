389c3e6eca518f749042ca63f0a7dedcb4fbb4bf
RojasCJP <jprojaschinchilla@gmail.com>
2022-02-25 17:13:54 -0600

    agregando flask-jwt

--- pythonServer/main.py
+++ pythonServer/main.py
@@ -1,4 +1,36 @@
 from flask import Flask, redirect, url_for
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
+
+class User(object):
+
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+
+users = [User(1, 'user1', 'user1pass'), User(2, 'user2', 'user2pass')]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(username, password: str):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'),
+                             password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
 
 app = Flask(__name__)
 app.debug = True
@@ -27,7 +59,7 @@ def photo():
 
 @app.route("/photo/<id>")  #put y delete
 def photoId(id):
-    return "pagina photo "+id
+    return "pagina photo " + id
 
 
 @app.route("/album")  #get
@@ -37,7 +69,7 @@ def album():
 
 @app.route("/album/<id>")  #delete
 def albumId(id):
-    return "pagina album "+ id
+    return "pagina album " + id
 
 
 if __name__ == "__main__":
