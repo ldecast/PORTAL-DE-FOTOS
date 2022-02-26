03430200b8af223a2f184564220feeb146a19614
RojasCJP <jprojaschinchilla@gmail.com>
2022-02-25 20:24:34 -0600

    listo lo de los webtokens solo es de ver como pasamos la informacion

--- pythonServer/main.py
+++ pythonServer/main.py
@@ -1,40 +1,27 @@
-from flask import Flask, redirect, url_for
-from flask_jwt import JWT, jwt_required, current_identity
-from werkzeug.security import safe_str_cmp
-
-
-class User(object):
-
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-
-users = [User(1, 'user1', 'user1pass'), User(2, 'user2', 'user2pass')]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-
-
-def authenticate(username, password: str):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'),
-                             password.encode('utf-8')):
-        return user
-
-
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
-
+import json
+from flask import Flask, jsonify, request, make_response
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'seminario10'
 #TODO tengo que quitar el app.debug para produccion
+
+
+def token_required(f):
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        if not token:
+            return jsonify({'message': 'token missing'}), 403
+
+        data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=['HS256'])
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route("/")
@@ -44,17 +31,35 @@ def home():
 
 @app.route("/login")  #post
 def login():
-    return "pagina login"
+    uname = "usuario"
+    password = "pass"
+    if uname == "usuario" and password == "pass":
+        token = jwt.encode(
+            {
+                'user': uname,
+                'exp':
+                datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+            }, app.config['SECRET_KEY'])
+        return jsonify({'token': token})
+
+    return 'pagina login'
 
 
 @app.route("/user")  #post, get y put
+@token_required
 def user():
     return "pagina user"
 
 
 @app.route("/photo")  #post
 def photo():
-    return "pagina photo"
+    auth = request.authorization
+
+    if auth and auth.password == 'password':  #tengo que cambiar el string por algo correcto
+        return ''
+
+    return make_response('not verifyed', 401,
+                         {'WWW-Authenticate': 'Login Required'})
 
 
 @app.route("/photo/<id>")  #put y delete
