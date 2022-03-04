3afcba2879d45e92f8f30b21ee74d7150797d368
RojasCJP <jprojaschinchilla@gmail.com>
2022-03-03 21:04:47 -0600

    merged x3

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,13 +1,8 @@
 import request from '@/helpers/request'
 
 export const loginUser = (user) => {
-<<<<<<< HEAD
-  return request('POST', '/login', user).then(({ data, headers }) => {
-    localStorage.setItem('faunaToken', headers['X-Access-Token'])
-=======
   return request('POST', '/login', user).then(({ data }) => {
     localStorage.setItem('faunaToken', headers['x-access-token'])
->>>>>>> f55582f69d0f6d4b03acc238ca7613dfc65a732f
     return data
   })
 }
