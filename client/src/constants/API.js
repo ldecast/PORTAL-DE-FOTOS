79140d28d76cdf49efb1e99508dd5b189499c77e
pabloc54p <pablofernando54@outlook.com>
2022-03-05 23:15:14 -0600

    production api fixed

--- client/src/constants/API.js
+++ client/src/constants/API.js
@@ -1,4 +1,4 @@
-const isProduction = import.meta.PROD
+const isProduction = import.meta.env.PROD
 console.log(`Running in ${isProduction ? 'production' : 'development'} mode`)
 export const API_URL = isProduction
   ? 'http://ec2-3-93-58-70.compute-1.amazonaws.com'
