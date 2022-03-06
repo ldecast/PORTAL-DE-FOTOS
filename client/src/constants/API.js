0c2de250f76b256524c371a5b0deff71eb71e33d
Pablo Cabrera <pablofernando54@outlook.com>
2022-03-06 12:42:26 -0600

    Update API.js

--- client/src/constants/API.js
+++ client/src/constants/API.js
@@ -1,5 +1,5 @@
 const isProduction = import.meta.env.PROD
 console.log(`Running in ${isProduction ? 'production' : 'development'} mode`)
 export const API_URL = isProduction
-  ? 'http://ec2-3-93-58-70.compute-1.amazonaws.com'
+  ? 'http://seminario-g10-1084886731.us-east-1.elb.amazonaws.com'
   : 'http://localhost:5000'
