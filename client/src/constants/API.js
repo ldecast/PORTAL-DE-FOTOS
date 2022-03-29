edf7e771d8ac9833258374bc42fdd83c467fa697
pabloc54p <pablofernando54@outlook.com>
2022-03-29 11:37:46 -0600

    api url fix

--- client/src/constants/API.js
+++ client/src/constants/API.js
@@ -1,4 +1,4 @@
 // const isProduction = import.meta.env.PROD
 const isProduction = true
 console.log(`Running in ${isProduction ? 'production' : 'development'} mode`)
-export const API_URL = isProduction ? 'http://54.89.223.177/' : 'http://localhost:5000'
+export const API_URL = isProduction ? 'http://18.212.19.143' : 'http://localhost:5000'
